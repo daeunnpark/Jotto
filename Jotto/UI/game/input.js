24f081a4e31775a11e902f16665980a0606ef480
mmstudent <mmstudent@mm46.local>
2019-02-23 17:55:56 -0500

    input checking added

--- Jotto/UI/game/input.js
+++ Jotto/UI/game/input.js
@@ -1,16 +1,40 @@
-const nameInput = document.getElementById("name");
-/*Now working only with first input field */
-/* not working with byClassName */
+const letter_1 = document.getElementById("letter_1");
+const letter_2 = document.getElementById("letter_2");
+const letter_3 = document.getElementById("letter_3");
+const letter_4 = document.getElementById("letter_4");
+const letter_5 = document.getElementById("letter_5");
 
-nameInput.addEventListener("input", () => {
-  nameInput.setCustomValidity("");
-  nameInput.checkValidity();
-});
 
-nameInput.addEventListener("invalid", () => {
-  if (nameInput.value === "") {
-    nameInput.setCustomValidity("Enter your guess.");
+letter_1.addEventListener("input", f1);
+letter_1.addEventListener("invalid", f2);
+
+
+letter_2.addEventListener("input", f1);
+letter_3.addEventListener("invalid", f2);
+
+
+letter_3.addEventListener("input", f1);
+letter_3.addEventListener("invalid", f2);
+
+
+letter_4.addEventListener("input", f1);
+letter_4.addEventListener("invalid", f2);
+
+
+letter_5.addEventListener("input", f1);
+letter_5.addEventListener("invalid", f2);
+
+
+
+function f1(){
+  this.setCustomValidity("");
+  this.checkValidity();
+}
+
+function f2(){
+  if (this.value === "") {
+    this.setCustomValidity("Enter your guess.");
   } else {
-    nameInput.setCustomValidity("Your guess can only contain letters.");
+    this.setCustomValidity("Your guess can only contain letters.");
   }
-});
+}
