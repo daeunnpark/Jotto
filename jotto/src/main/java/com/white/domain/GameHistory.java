d403fd9449f3ec8d3b9c4dd6023f56551798dad8
Himchan Yun <himchan.yun@stonybrook.edu>
2019-02-26 15:08:04 -0500

    added

--- jotto/src/main/java/com/white/domain/GameHistory.java
+++ jotto/src/main/java/com/white/domain/GameHistory.java
@@ -1,4 +1,21 @@
 package com.white.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class GameHistory {
+
+    @EmbeddedId
+    private GameHistoryId id;
+
+    @Embeddable
+    public class GameHistoryId{
+        int userID;
+        int Date;
+    }
+
 }
+
+
+
+
