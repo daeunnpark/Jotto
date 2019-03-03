b7307d22932e53ea4538dd0747753e7ce5b137ae
Seok Hoon Kim <SeokHoonKim@Seoks-MBP.fios-router.home>
2019-03-03 08:20:42 -0500

    cant resolve

--- demo/src/main/java/com/white/demo/database/GameController/GameController.java
+++ demo/src/main/java/com/white/demo/database/GameController/GameController.java
@@ -7,15 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.Model;
 
 
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 import com.white.demo.database.model.GameLog;
 import com.white.demo.database.model.Game;
 import com.white.demo.database.model.Login;
 import com.white.demo.database.Repositories.GameLogRepository;
 import com.white.demo.database.LoginController.LoginController;
-
+import java.util.ArrayList;
 
 import com.white.demo.database.Repositories.GameRepository;
 import com.white.demo.database.Repositories.LoginRepository;
@@ -38,7 +42,7 @@ public class GameController {
 
     @GetMapping(path="/addGame") // Map ONLY GET Requests
     public @ResponseBody String addNewGame (@RequestParam int game_ID,
-            @RequestParam String Player1, @RequestParam String Player2) {
+                                            @RequestParam String Player1, @RequestParam String Player2) {
         // @ResponseBody means the returned String is the response, not a view name
         // @RequestParam means it is a parameter from the GET or POST request
 
@@ -53,10 +57,19 @@ public class GameController {
         return "GameCount: " + gamecount++;
 
     }
-    @GetMapping(path="/history")
-    public String showAll(Model model) {
-        model.addAttribute("games", gameRepository.findByplayer1(LoginController.name));
-        return "redirect:/history";
+
+    @RequestMapping(value = "/history", method = RequestMethod.GET)
+    public
+    ModelAndView
+    showAll() {
+        ModelAndView mv =  new ModelAndView("redirect:/history");
+        //System.out.println(gameRepository.findAll().getClass());
+        List<Game> g = gameRepository.findByplayer1(LoginController.name);
+        mv.addObject("games", g);
+        // for (Game g :  gameRepository.findByplayer1(LoginController.name)){
+        //    System.out.println(g.getPlayer1());
+        // }
+        return mv;
     }
 
 
