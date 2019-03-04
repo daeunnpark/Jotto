bee9beb34705019b3b638a22f25d065cfcf2f049
daeunnpark <daeunn@gmail.com>
2019-03-03 23:10:19 -0500

    submitbtn2 eventlistener click changed to submit to not allow invalid input format

--- demo/src/main/resources/static/js/game.js
+++ demo/src/main/resources/static/js/game.js
@@ -21,7 +21,7 @@ var firstTimePlaying = 1;
 //for legal_words.txt
 var dictArray = [];
 var word_list_URL =
-    "https://raw.githubusercontent.com/daeunnpark/daeunnpark.github.io/master/legal_words.txt";
+  "https://raw.githubusercontent.com/daeunnpark/daeunnpark.github.io/master/legal_words.txt";
 
 // Global variables end
 
@@ -80,25 +80,34 @@ $("#inputform2").submit(function(event) {
   return false; // avoid to execute the actual submit of the form.
 });
 
-document.getElementById("submitbtn2").addEventListener("click", function() {
+document.getElementById("inputform2").addEventListener("submit", function() {
+  alert("submitted");
   user_guess();
   computerGuess(currentArray);
 });
 
+//"#inputform2").submit
+/*
+document.getElementById("submitbtn2").addEventListener("submit", function() {
+  alert("submitted");
+  user_guess();
+  computerGuess(currentArray);
+});
+*/
 document.querySelectorAll(".letterboard td").forEach(e =>
-e.addEventListener("click", function() {
-  if (this.style.backgroundColor == "green") {
-    this.style.backgroundColor = "red";
-  } else if (this.style.backgroundColor == "red") {
-    this.style.backgroundColor = "white";
-  } else if (this.style.backgroundColor == "white") {
-    this.style.backgroundColor = "green";
-  } else {
-    // first click
-    this.style.backgroundColor = "green";
-  }
-  color_code(this.textContent.trim(), this.style.backgroundColor);
-})
+  e.addEventListener("click", function() {
+    if (this.style.backgroundColor == "green") {
+      this.style.backgroundColor = "red";
+    } else if (this.style.backgroundColor == "red") {
+      this.style.backgroundColor = "white";
+    } else if (this.style.backgroundColor == "white") {
+      this.style.backgroundColor = "green";
+    } else {
+      // first click
+      this.style.backgroundColor = "green";
+    }
+    color_code(this.textContent.trim(), this.style.backgroundColor);
+  })
 );
 
 function color_code(letter, color) {
@@ -123,13 +132,13 @@ window.alert(
       for (var i = 0; i < str.length; i++) {
         if (str.charAt(i) === letter) {
           table.rows[r].cells[1].innerHTML =
-              str.substring(0, i) +
-              '<span style="background-color:' +
-              color +
-              '">' +
-              str.charAt(i) +
-              "</span>" +
-              str.substring(i + 1);
+            str.substring(0, i) +
+            '<span style="background-color:' +
+            color +
+            '">' +
+            str.charAt(i) +
+            "</span>" +
+            str.substring(i + 1);
         }
       }
     }
@@ -137,12 +146,12 @@ window.alert(
   }
 }
 
-function userG(){
+function userG() {
   var table = document.getElementById("userGuessTable");
-  printGuess(table,1,"hello",1);
+  printGuess(table, 1, "hello", 1);
 }
 /* User function start */
-function user_guess(){
+function user_guess() {
   var table = document.getElementById("userGuessTable");
   var char1 = document.getElementById("letter_1").value;
   var char2 = document.getElementById("letter_2").value;
@@ -150,69 +159,76 @@ function user_guess(){
   var char4 = document.getElementById("letter_4").value;
   var char5 = document.getElementById("letter_5").value;
 
-  if(!(char1 === "" || char2 === "" || char3 === "" || char4 === "" || char5 === "")){
+  if (
+    !(
+      char1 === "" ||
+      char2 === "" ||
+      char3 === "" ||
+      char4 === "" ||
+      char5 === ""
+    )
+  ) {
     var myGuess = char1 + char2 + char3 + char4 + char5;
 
     numUserGuess++;
     userGuessList.push(myGuess);
-    var count1 = count("grave","basic");
+    var count1 = count("grave", "basic");
     userCorrectCountList.push(count1);
-    printGuess(table,numUserGuess,myGuess,count1);
+    printGuess(table, numUserGuess, myGuess, count1);
     checkGuess(myGuess, userWord, "user");
   }
 }
 
 /* User function end */
 
-
 /* Computer function start */
 
-function computerInit(){
+function computerInit() {
   computerWord = random(dictArray);
   currentArray = dictArray;
 }
 
-function computerGuess(current_array){
+function computerGuess(current_array) {
   numComGuess++;
   var guess = random(current_array);
   computerGuessList.push(guess);
-  var count1 = count(guess,computerWord);
+  var count1 = count(guess, computerWord);
   computerCorrectCountList.push(count);
   var table = document.getElementById("computerGuessTable");
-  printGuess(table,numComGuess,guess,count1);
+  printGuess(table, numComGuess, guess, count1);
   checkGuess(guess, computerWord, "computer");
   currentArray = refine_array(computerWord, count1, current_array);
 }
 
-function random(arr){
-  return arr[Math.floor(Math.random()*arr.length)];
+function random(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
-function count(word1, word2){
-  var charArr1 = word1.split('');
-  var charArr2 = word2.split('');
+function count(word1, word2) {
+  var charArr1 = word1.split("");
+  var charArr2 = word2.split("");
   var i;
   var count = 0;
-  for(i=0; i<charArr1.length; i++){
-    if(charArr2.includes(charArr1[i])){
+  for (i = 0; i < charArr1.length; i++) {
+    if (charArr2.includes(charArr1[i])) {
       count++;
     }
   }
   return count;
 }
 
-function refine_array(word, count, array){
+function refine_array(word, count, array) {
   var temp = [];
   var i;
-  for(i=0; i<array.length; i++){
-    if(count(word,array[i]) == count){
+  for (i = 0; i < array.length; i++) {
+    if (count(word, array[i]) == count) {
       temp.push(array[i]);
     }
   }
   return temp;
 }
 
-function printGuess(table, numGuess, guess, count){
+function printGuess(table, numGuess, guess, count) {
   var row = table.insertRow(table.rows.length);
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
@@ -223,19 +239,19 @@ function printGuess(table, numGuess, guess, count){
   cell3.innerHTML = count;
 }
 
-function checkGuess(guess, word, player){
-  if(guess === word){
+function checkGuess(guess, word, player) {
+  if (guess === word) {
     winner = player;
     saveHistory();
     popUpWinner();
   }
 }
 
-function saveHistory(){
+function saveHistory() {
   //send the game result to DB
 }
 
-function popUpWinner(){
+function popUpWinner() {
   //pop up winner window
   //end the game
 }
